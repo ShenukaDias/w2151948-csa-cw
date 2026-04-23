799ca30e48fcf083f75781ea5d355f6da70caed9
shenukadias <shenukadias271@gmail.com>
2026-04-24 01:24:46 +0530

    Align bootstrap with JAX-RS application path

--- src/main/java/com/mycompany/smartcampusapi/SmartCampusApplication.java
+++ src/main/java/com/mycompany/smartcampusapi/SmartCampusApplication.java
@@ -1,6 +1,8 @@
 package com.mycompany.smartcampusapi;
 
+import jakarta.ws.rs.ApplicationPath;
 import jakarta.ws.rs.core.Application;
 
+@ApplicationPath("/api/v1")
 public class SmartCampusApplication extends Application {
 }
